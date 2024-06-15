06f225caac9b570e39a0604d7f2093693d38fc9e
Filipe Novaes <lfns3@aluno.ifal.edu.br>
2024-06-15 15:38:48 -0300

    mudancinhas leves no gpt: falta comentar

--- letterboxd_scraping/gpt.py
+++ letterboxd_scraping/gpt.py
@@ -4,45 +4,48 @@ import pandas as pd
 file = open("gpt_api.txt", "r")
 key_gpt = file.read()
 
-client = ai.OpenAI(api_key=key_gpt)
+client = ai.OpenAI(api_key="sk-proj-E654f2NNIYhVnMSuWRzvT3BlbkFJrAidSyfoXD0raVkttQhi")
 
 # TODO: TESTAR!!!
 
 def get_response(prompt, max_tokens=150, model="gpt-3.5-turbo-0125", temperature = 0):
     response = client.chat.completions.create(
-        messages = prompt, # ou prompt = prompt? TODO: VERIFICAR
+        messages = [{'role': 'user', 'content': prompt}],
         model = model,
         max_tokens = max_tokens,
         temperature = temperature
         )
+    
     return response.choices[0].message.content
 
 
-def summary_based_on_reviews(df):
-    # TODO: REVISAR E MELHORAR E COMENTAR
-    reviews = df["Review"].tolist()
-    prompt = "Based on user reviews on Letterboxd, give me a brief summary of this movie:\n"
+def append_reviews_to_prompt(prompt, reviews):
     for review in reviews:
         prompt += f"- {review}\n"
+
+    return prompt
+
+def summary_based_on_reviews(reviews):
+    # TODO: REVISAR E MELHORAR E COMENTAR
+    prompt = "Based on user reviews on Letterboxd, give me a brief summary of this movie:\n"
+    prompt = append_reviews_to_prompt(prompt, reviews)
+
     return get_response(prompt)
 
 
-def summary_public_opinion(df):
+def summary_public_opinion(mean, reviews):
     # TODO: REVISAR E MELHORAR E COMENTAR
-    mean = df["Scores"].mean()
-    reviews = df["Review"].tolist()
     prompt = f"""Public opinion on the movie based on:\n
     Average score: {mean:.2f}\n
     and the following reviews:\n"""
-    for review in reviews:
-        prompt += f"- {review}\n"
+    prompt = append_reviews_to_prompt(prompt, reviews)
+
     return get_response(prompt)
 
 
-def keywords_for_movie(df):
+def keywords_for_movie(reviews):
     # TODO: REVISAR E MELHORAR E COMENTAR
-    reviews = df["Review"].tolist()
     prompt = "Keywords that describe this movie based on its reviews:\n"
-    for review in reviews:
-        prompt += f"- {review}\n"
+    prompt = append_reviews_to_prompt(prompt, reviews)
+
     return get_response(prompt)
--- letterboxd_scraping/main.py
+++ letterboxd_scraping/main.py
@@ -2,6 +2,7 @@
 
 import sys
 import letterboxd_bs4 as letterboxd
+from gpt import summary_based_on_reviews, summary_public_opinion, keywords_for_movie
 
 # Pega a url e a quantidade de páginas para analisar da linha de comando
 try:
@@ -20,6 +21,12 @@ except:
 df = letterboxd.get_data(url, page_ammount)
 
 # TODO: GPT IMPLEMENTATION
+reviews = df["Review"].tolist()
+mean = df["Score"].mean()
+
+df["Summary"] = summary_based_on_reviews(reviews)
+df["Public_Opinion"] = summary_public_opinion(mean, reviews)
+df["Keywords"] = keywords_for_movie(reviews)
 
 # Função que cria o arquivo csv com os dados
 letterboxd.create_csv(url, df)
